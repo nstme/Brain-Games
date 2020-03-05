1d722d903bbbfd65be6df9bad9baea98efcfdb90
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-03-04 22:40:13 -0800

    add brain-even game

---
+++ games/isEven.js
@@ -0,0 +1,14 @@
+import initGame from '../src/index.js';
+
+const gameTask = 'Answer \x1b[31m"yes"\x1b[0m if the number is even, otherwise answer \x1b[31m"no"\x1b[0m.';
+
+const isEven = (x) => x % 2 === 0;
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const getAnswer = (num) => {
+  if (isEven(num)) return 'yes';
+  return 'no';
+};
+
+export default () => initGame(gameTask, getRandomNumber, getAnswer);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+export default (gameTask, getRandomNumber, getAnswer) => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  if (!gameTask) return;
+  console.log(gameTask);
+
+  let correctUserAnswerCount = 0;
+  const questionsCount = 3;
+  for (let i = 0; i < questionsCount; i += 1) {
+    const randomNumber = getRandomNumber(1, 500);
+    const correctAsnwer = getAnswer(randomNumber);
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAsnwer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer is "${correctAsnwer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    if (userAnswer === correctAsnwer) {
+      correctUserAnswerCount += 1;
+      console.log('Correct!');
+    }
+    if (correctUserAnswerCount === questionsCount) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
 };
-export default greeting;
