aaeab91185dc8a95ace567619f390986ff2bb06a
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-03-07 20:16:54 -0800

    add isPrime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import isPrime from '../games/isPrime.js';
+
+isPrime();
---
+++ games/isPrime.js
@@ -0,0 +1,22 @@
+import initGame from '../src/index.js';
+
+const gameTask = 'Answer \x1b[31m"yes"\x1b[0m if given number is prime, otherwise answer \x1b[31m"no"\x1b[0m.';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const isPrime = (num) => {
+  if (num === 1) return true;
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getAnswer = (num) => {
+  if (isPrime(num)) return 'yes';
+  return 'no';
+};
+
+export default () => initGame(gameTask, getRandomNumber, getAnswer);
