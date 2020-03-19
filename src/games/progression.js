364da0b5e16e3320e46570d37066f352b0cf701a
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-03-18 19:53:10 -0700

    refactor progression game: move question logic to getGameData func

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,47 +11,20 @@ const getProgression = (minNum, maxNum) => {
   const start = getRandomNumber(minNum, maxNum);
   const step = getRandomNumber(2, 10);
   const end = start + step * progressionLength;
-  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
   const progression = [];
   for (let i = start; i < end; i += step) {
     progression.push(i);
   }
-  progression[hiddenIndex] = '..';
-  return progression.join(' ');
-};
-
-const getAnswer = (progressionStr) => {
-  let answer = 0;
-  const progressionArr = progressionStr.split(' ');
-  for (let i = 0; i < 10; i += 1) {
-    const current = progressionArr[i];
-    const prev = Number(progressionArr[i - 1]);
-    const prevPrev = Number(progressionArr[i - 2]);
-    const next = Number(progressionArr[i + 1]);
-    const nextNext = Number(progressionArr[i + 2]);
-    let step;
-
-    if (progressionArr[0] === '..') {
-      step = nextNext - next;
-      answer = next - step;
-      return answer.toString();
-    }
-    if (current === '..') {
-      answer = (prev + next) / 2;
-      return answer.toString();
-    }
-    if (progressionArr[progressionArr.length - 1] === '..') {
-      step = prev - prevPrev;
-      answer = prev + step;
-      return answer.toString();
-    }
-  }
-  return 'error';
+  return progression;
 };
 
 const getGameData = () => {
-  const question = getProgression(min, max);
-  const answer = getAnswer(question);
+  const progression = getProgression(min, max);
+  const hiddenIndex = getRandomNumber(0, progression.length - 1);
+  const temp = [...progression];
+  temp[hiddenIndex] = '..';
+  const question = temp.join(' ');
+  const answer = progression[hiddenIndex].toString();
   return [question, answer];
 };
 
